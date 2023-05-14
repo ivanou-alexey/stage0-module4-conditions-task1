b488842af12a3c74720e6ea6e7004dc9b92f91fe
Alexey Ivanov <ai5973861@gmail.com>
2023-05-14 23:51:49 +0300

    Fix

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -3,9 +3,9 @@ package school.mjc.stage0.conditions.task1;
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
         if (0 > 1){
-            System.out.println("true");
-        } else {
             System.out.println("false");
+        } else {
+            System.out.println("true");
         }
     }
 }
